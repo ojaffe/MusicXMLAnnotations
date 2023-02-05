ea4bf000b9b1e2667c38d606369f6e48327c7d4a
Oliver Jaffe <oliver.jaffe@hotmail.co.uk>
2023-02-05 20:00:25 +0000

    Fixed timings from first bar

--- genannotations.py
+++ genannotations.py
@@ -4,7 +4,7 @@ import argparse
 from musicxml import MusicXML
 
 
-def filterForAnnotations(sequences):
+def filterForAnnotations(sequences, include_notes=False, include_rests=False):
     annotations = list()
 
     for seq in sequences:
@@ -13,20 +13,25 @@ def filterForAnnotations(sequences):
         for elem in s:
             tokens = elem.split(' ')
             t = tokens[0]  # Take first element in chord. They will all have same length anyway
-            t = t.replace('.', '')
 
             if 'barline' in t:
                 annotations.append(t)
             elif 'note' in t:
-                continue
-                split = t.split('_')
-                time = '_'.join(split[1:len(split) + 1])
-                annotations.append(time)
+                if include_notes:
+                    split = t.split('_')
+                    time = '_'.join(split[1:len(split) + 1])
+                    annotations.append(time)
+                else:
+                    continue
+
             elif 'rest' in t:
-                continue
-                split = t.split('-')
-                time = '-'.join(split[1:len(split) + 1])
-                annotations.append(time)
+                if include_rests:
+                    split = t.split('-')
+                    time = '-'.join(split[1:len(split) + 1])
+                    annotations.append(time)
+                else:
+                    continue
+                
             elif 'clef' in t or 'key' in t:
                 continue
             elif '+' in t:  # If bar empty
@@ -41,12 +46,12 @@ def get_bar_annotations(staves):
     staves_bars = list()
     for stave in staves:
         sequences = [x for x in stave if x != '']
-        sequences_annotations = filterForAnnotations(sequences)
+        sequences_annotations = filterForAnnotations(sequences, include_notes=False, include_rests=False)
 
         # Group all elements in same bars together
         full = list()
         bar = list()
-        for s in sequences_annotations[1:]:
+        for s in sequences_annotations:
             if s == 'barline':
                 full.append(bar)
                 bar = list()
@@ -64,26 +69,59 @@ def get_bar_annotations(staves):
             else:
                 merged.append(bar)
 
-    merged = [[sequences_annotations[0]]] + merged
     return merged
 
 
-def calculateAnnotationTimes(sequence):
-    time_dict = {
+def get_first_bar_time(sequence):
+    first_bar = [x for x in sequence if x != ''][0]
+    first_bar = first_bar.split(' + barline + ')[0]  # Get first bar
+    first_bar = filterForAnnotations([first_bar], include_notes=True, include_rests=True)
+    
+    note_dur_dict = {'half': 1/2,
+            'quarter': 1/4,
+            'eighth': 1/8,
+            'sixteenth': 1/16,
+            'thirty_second': 1/32}
+
+    counter = 0
+    for t in first_bar:
+        dot = True if '.' in t else False
+        t = t.replace('.', '')
+        if t in note_dur_dict.keys():
+            if dot:
+                counter += note_dur_dict[t] * 1.5
+            else:
+                counter += note_dur_dict[t]
+
+    return counter
+
+def calculateAnnotationTimes(staves, sequence):
+    # Get time of first bar in top stave
+    first_bar_time = get_first_bar_time(staves[0])
+
+    time_sig_dict = {
+        'C/': 1,
         '4/8': 4/8,
         '3/8': 3/8,
     }
 
+    add_next = 0
     time_sig = 0
     running_time = 0
     annotation_times = list()
-    for bar in sequence:
+    for idx, bar in enumerate(sequence):
         running_time += time_sig
+
+        running_time += add_next
+        add_next = 0
 
         for elem in bar:
             if 'timeSignature' in elem:
                 bar_time_sig_end = elem.split('-')[1]
-                time_sig = time_dict[bar_time_sig_end]
+                time_sig = time_sig_dict[bar_time_sig_end]
+
+                if idx == 0:
+                    add_next = first_bar_time - time_sig
             else:
                 annotation_times.append((running_time, elem))
 
@@ -109,7 +147,7 @@ def gen_annotations(args):
         print('Merged annotations:\n', merged)
 
     if args.time:
-        merged = calculateAnnotationTimes(merged)
+        merged = calculateAnnotationTimes(staves, merged)
         if args.verbose:
             print('Times:\n', merged)
     
