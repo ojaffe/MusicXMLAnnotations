7dbd44ffe9933dab3e11f9c6d0816f9321f08473
Oliver Jaffe <oliver.jaffe@hotmail.co.uk>
2023-02-05 15:56:32 +0000

    Converts single file

--- genannotations.py
+++ genannotations.py
@@ -8,7 +8,6 @@ def filterForAnnotations(sequences):
     annotations = list()
 
     for seq in sequences:
-        annotations.append("a")
         s = seq.split(' + ')  # Each element is a series of simultaneous tokens
 
         for elem in s:
@@ -38,72 +37,71 @@ def filterForAnnotations(sequences):
     return annotations
 
 
-def collapseAnnotations(sequence):
-    len_dict = {'half': 1/2,
-                'quarter': 1/4,
-                'eighth': 1/8,
-                'sixteenth': 1/16,
-                'thirty_second': 1/32}
-    counter = 0
-    key = None
-    for t in sequence:
-        if 'time' in t:
-            time = t.split('-')[1].split('/')
-            time = int(time[0]) / int(time[1])
-            print('TIME', time)
+def get_bar_annotations(staves):
+    staves_bars = list()
+    for stave in staves:
+        sequences = [x for x in stave if x != '']
+        sequences_annotations = filterForAnnotations(sequences)
 
-        if t in len_dict.keys():
-            counter += len_dict[t]
+        # Group all elements in same bars together
+        full = list()
+        bar = list()
+        for s in sequences_annotations[1:]:
+            if s == 'barline':
+                full.append(bar)
+                bar = list()
+            elif 'timeSignature' not in s:
+                bar.append(s)
 
-        if t == 'barline':
-            print(counter)
-            counter = 0
+        staves_bars.append(full)
+
+    # Merge
+    merged = list()
+    for s in staves_bars:
+        for idx, bar in enumerate(s):
+            if len(merged) > idx:
+                merged[idx] += bar
+            else:
+                merged.append(bar)
+
+    merged = [[sequences_annotations[0]]] + merged
+    return merged
+
+
+def gen_annotations(args):
+    musicxml_obj = MusicXML(input_file=args.input)
+
+    try:
+        # Write sequence
+        sequences = musicxml_obj.get_sequences()
+        staves = [[x[0] for x in sequences]] + [[x[1] for x in sequences]]
+
+        # Check staves have same length
+        if len(staves[0]) * len(staves) != sum([len(s) for s in staves]):
+            raise Exception('Decoded staves have different lengths')
+
+        merged = get_bar_annotations(staves)
+        if args.verbose:
+            print('Merged annotations:\n', merged)
+        
+        return merged
+    
+    except UnicodeDecodeError: # Ignore bad MusicXML
+        raise Exception('Corrupted file')
+
 
 if __name__ == '__main__':
-
     """
     Command line args:
 
     -input <input directory with MusicXMLS>
+    --verbose
     """
 
     # Parse command line arguments for input/output directories
     parser = argparse.ArgumentParser()
     parser.add_argument('-input', dest='input', type=str, required='-c' not in sys.argv, help='Path to the input directory with MusicXMLs.')
+    parser.add_argument('--verbose', dest='verbose', action='store_true')
     args = parser.parse_args()
 
-    # Track all unique tokens
-    pitch_tokens = list()
-    rhythm_tokens = list()
-
-    # Go through all inputs generating output sequences
-    for i, file_name in enumerate(os.listdir(args.input)):
-
-        # Ignore non .musicxml files
-        if not file_name.endswith('.musicxml'):
-            continue
-
-        # Create a MusicXML object for generating sequences
-        input_path = os.path.join(args.input, file_name)
-        musicxml_obj = MusicXML(input_file=input_path)
-
-        try:
-            # Write sequence
-            sequences = musicxml_obj.get_sequences()
-            staves = [[x[0] for x in sequences]] + [[x[1] for x in sequences]]
-            print(len(staves[0]), len(staves[1]))
-            print(staves[0], staves[1])
-
-            # Add new unique tokens to vocab
-            sequences = [x for x in staves[0] if x != '']
-            sequences_annotations = filterForAnnotations(sequences)
-            print(sequences_annotations)
-            print(len([0 for x in sequences_annotations if x == 'barline']))
-
-            sequences = [x for x in staves[1] if x != '']
-            sequences_annotations = filterForAnnotations(sequences)
-            print(sequences_annotations)
-            print(len([0 for x in sequences_annotations if x == 'barline']))
-        
-        except UnicodeDecodeError: # Ignore bad MusicXML
-            pass  # TODO nuke bad files
+    gen_annotations(args)
